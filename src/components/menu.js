7418f44d2aec63f63380e16d2d6e348130254989
DiegoT2k <87203188+DiegoT2k@users.noreply.github.com>
2024-05-07 15:44:10 +0200

    Test

--- src/components/menu.js
+++ src/components/menu.js
@@ -25,7 +25,7 @@ class Menu extends Component{
         return(
         <>
             <div className="container">
-                <h1>Quale gita vuoi fare?</h1>
+                <h1>Quale gitaasdads vuoi fare?</h1>
                 <hr/>
                 <div className="row">
                     {this.state.cards.map(card => (
