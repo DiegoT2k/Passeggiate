d7df5720e652c37956cac487dff27882f486531b
DiegoT2k <87203188+DiegoT2k@users.noreply.github.com>
2024-05-09 15:12:24 +0200

    Edit

--- src/components/lago.js
+++ src/components/lago.js
@@ -1,56 +1,26 @@
+import React, { useEffect } from 'react';
+
 import Navbar from "./navbar";
-import foto1 from "../images/foto1.jpg";
-import foto2 from "../images/foto1.jpg";
-import foto3 from "../images/foto1.jpg";
+import foto from "../images/foto5.webp";
+
 
 function Lago(){
+    
     return(
         <>
             <Navbar></Navbar>
-            <h1 style={{textAlign: "center", padding: ".7vmax", fontFamily: "Marina Sikoka" }}>Lago d'arpy</h1>
-            <div className="container">
-
-                <div id="carouselExampleCaptions" className="carousel slide carousel-fade">
-                <div className="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                </div>
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                    <img src={foto1} className="d-block w-100" alt="..."></img>
-                    <div className="carousel-caption d-none d-md-block">
-                        <h5>First slide label</h5>
-                        <p>Some representative placeholder content for the first slide.</p>
-                    </div>
-                    </div>
-                    <div className="carousel-item">
-                    <img src={foto2} className="d-block w-100" alt="..."></img>
-                    <div className="carousel-caption d-none d-md-block">
-                        <h5>Second slide label</h5>
-                        <p>Some representative placeholder content for the second slide.</p>
-                    </div>
-                    </div>
-                    <div className="carousel-item">
-                    <img src={foto3} className="d-block w-100" alt="..."></img>
-                    <div className="carousel-caption d-none d-md-block">
-                        <h5>Third slide label</h5>
-                        <p>Some representative placeholder content for the third slide.</p>
-                    </div>
-                    </div>
-                </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Previous</span>
-                </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Next</span>
-                </button>
-                </div>   
-
+            <div className='container'>
+                <h1 style={{textAlign:"center"}}>Lago d'arpy</h1>
+                <img className='img-fluid mx-auto p-2 rounded d-block' src={foto} style={{height:"50vh"}}></img>  
+                <p>
+                <b>Durata:</b> 1h <br></br>
+                <b>Percorso:</b>Est interdum praestare mercaturis rem quaerere, nisi tam periculosum sit, et item foenerari, si tam honestum. Maiores nostri sic habuerunt et ita in legibus posiverunt: furem dupli condemnari, foeneratorem quadrupli. Quanto peiorem civem existimarint foeneratorem quam furem, hinc licet existimare. Et virum bonum quom laudabant, ita laudabant: bonum agricolam bonumque colonum; amplissime laudari existimabatur qui ita laudabatur. Mercatorem autem strenuum studiosumque rei quaerendae existimo, verum, ut supra dixi, periculosum et calamitosum. At ex agricolis et viri fortissimi et milites strenuissimi gignuntur, maximeque pius quaestus stabilissimusque consequitur minimeque invidiosus, minimeque male cogitantes sunt qui in eo studio occupati sunt. Nunc, ut ad rem redeam, quod promisi institutum principium hoc erit.             
+                <br></br>
+                <b>Acqua:</b> fontanelle durante il percorso <br></br>
+                <b>Fauna:</b> scarsità di fauna, magari qualche capriolo <br></br>
+                <b>Consigli:</b><br></br>
+                </p>
             </div>
-        
         </>
     );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+
+
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import Contatti from './components/contatti';
